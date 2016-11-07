32f72a06f8de0646249f577972003a06db17155a
Dibya Chakravorty <dibyachakravorty@gmail.com>
2016-11-07 14:38:45 +0100

    updated description and changed single quotes to double quotes

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='word_forms',
-      version='1.0',
-      description='Generate different forms of a word.',
-      author='Dibya Chakravorty',
-      author_email='dibyachakravorty@gmail.com',
-      packages=['word_forms'],
-      package_data={'word_forms' : ['en-verbs.txt']},
+setup(name="word_forms",
+      version="1.0",
+      description="Generate all possible forms of an English word.",
+      author="Dibya Chakravorty",
+      author_email="dibyachakravorty@gmail.com",
+      packages=["word_forms"],
+      package_data={"word_forms" : ["en-verbs.txt"]},
       include_package_data=True,
       install_requires = ["Unipath==1.1", "inflect==0.2.5", "nltk==3.2.1"]
       )
